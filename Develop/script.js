1df76897c262078a41276801ce0a5257e458e386
erikmarquez <erikmar3@gmail.com>
2020-07-11 16:58:34 -0500

    added event listener

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,16 @@ $("document").ready(function(){
 
     $("#currentDay").text(moment().format("dddd, MMMM do"));
 
-    console.log(moment().format("hA"))
+    // console.log(moment().format("hA"))
 
     var current = moment().format("hA")
 
     var present = "1"
-    var future= "2"
-    var past = "12"
+    var future= "5PM"
+    var past = "3PM"
 
     // console.log( moment().isBefore(present, "hour"))
-    console.log(  moment().hour(9).format("hA"))
+    // console.log(  moment().hour(9).format("hA"))
 
     function timeBlocks(){
 
@@ -25,9 +25,9 @@ $("document").ready(function(){
         row.append(hour,text,button)
 
         var timeSlots = 9
-        var startTime = 9
+        var startTime = 13
         for(var i = 0; i < timeSlots; i++){
-            console.log(i)
+            // console.log(i)
 
             var time = moment().hour(startTime).format("hA")
             var row = $("<div>").attr("class", "row time-block")
@@ -37,12 +37,26 @@ $("document").ready(function(){
             var button = $("<button>").attr("class", "col-1 saveBtn")
             var lock = $("<i>").attr("class", "fa fa-lock")
             button.append(lock)
+
+            if(current > time){
+                var text = $("<textarea>").attr({"class":"col-10 past", name:"", id:"", rows:"1" })
+            }
+            else if(current < time){
+                var text = $("<textarea>").attr({"class":"col-10 future", name:"", id:"", rows:"1" })
+
+            }
+
+            else if(current == time) {
+                var text = $("<textarea>").attr({"class":"col-10 present", name:"", id:"", rows:"1" })
+
+            }
+
     
             row.append(hour,text,button)
             $(".container").append(row)
 
             startTime++
-            console.log(startTime)
+            // console.log(startTime)
         }
 
 
@@ -50,6 +64,27 @@ $("document").ready(function(){
 
     }
 
+    function timeCheck(){
+
+        console.log(current == future)
+
+        
+
+
+
+    }
+
+
+
     timeBlocks()
 
+    
+    $(".saveBtn").click(function(event){
+
+        var toDO = $(this).parent().find("textarea")
+        VAR 
+
+
+        console.log(toDO[0].value)
+    })
 })
